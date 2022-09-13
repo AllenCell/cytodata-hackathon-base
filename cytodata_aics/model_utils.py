354bf082f98e1f8c2a18d1aae769f72d98979f39
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-09-13 18:34:50 +0000

    add a save_predictions function

---
+++ cytodata_aics/model_utils.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import pandas as pd
+
+def save_predictions_classifier(preds, output_dir):
+    """
+    TODO: make this better? maybe use vol predictor code?
+    """
+    records = []
+    for pred in preds:
+        record = dict()
+        for col in ["id", "y", "yhat"]:
+            record[col] = pred[col].numpy()
+        record["loss"] = [pred["loss"].item()] * len(pred["id"])
+        records.append(record)
+
+    pd.DataFrame.from_records(records).to_csv(
+        Path(output_dir) / "model_predictions.csv"
+    )
