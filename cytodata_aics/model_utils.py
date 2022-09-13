baa553e798fc0fcc7f021531f303a32b33b7874a
Guilherme Pires <guilherme.pires@alleninstitute.org>
2022-09-13 18:57:12 +0000

    fix save_predictions

--- cytodata_aics/model_utils.py
+++ cytodata_aics/model_utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 def save_predictions_classifier(preds, output_dir):
     """
     TODO: make this better? maybe use vol predictor code?
+    TODO: drop unnecessary index
     """
     records = []
     for pred in preds:
@@ -11,8 +12,8 @@ def save_predictions_classifier(preds, output_dir):
         for col in ["id", "y", "yhat"]:
             record[col] = pred[col].squeeze().numpy()
         record["loss"] = [pred["loss"].item()] * len(pred["id"])
-        records.append(record)
+        records.append(pd.DataFrame(record))
 
-    pd.DataFrame.from_records(records).to_csv(
-        Path(output_dir) / "model_predictions.csv"
+    pd.concat(records).reset_index().drop(columnd="index").to_csv(
+        Path(output_dir) / "model_predictions.csv", index_label=False
     )
